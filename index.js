e6f9d655ed3fc69a7487064d6bc1b6dfae905ead
DetlefDmann <dumpelmann.detlef@gmail.com>
2021-01-05 12:01:11 +0100

    most functionality

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const colorYellow = document.querySelector(".color__yellow");
 const colorGreen = document.querySelector(".color__green");
 const colorBlue = document.querySelector(".color__blue");
 const colorPurple = document.querySelector(".color__purple");
+const text = document.querySelector(".text");
 
 const showMenu = () => {
   menu.classList.remove("color__menu--hide");
   menu.classList.add("color__menu");
-  // clearTimeout(delayhideMenu);
 };
 const hideMenu = () => {
   menu.classList.remove("color__menu");
@@ -33,38 +33,110 @@ const setColor = (color) => {
   document.querySelector("body").classList.add(color);
 };
 
-//let delayhideMenu = () => setTimeout(hideMenu, 1000);
+const setText = (e) => {
+  text.innerHTML = `Background color selected: ${e}`;
+};
 
 hamburger.addEventListener("mouseover", showMenu);
 hamburger.addEventListener("mouseout", hideMenu);
-menu.addEventListener("mouseover", showMenu);
-menu.addEventListener("mouseout", hideMenu);
+
+const keyHandler = (e) => {
+  switch (e) {
+    case "1":
+      colorHandler("gray");
+      break;
+    case "2":
+      colorHandler("red");
+      break;
+    case "3":
+      colorHandler("orange");
+      break;
+    case "4":
+      colorHandler("yellow");
+      break;
+    case "5":
+      colorHandler("green");
+      break;
+    case "6":
+      colorHandler("blue");
+      break;
+    case "7":
+      colorHandler("purple");
+      break;
+    default:
+      break;
+  }
+};
+
+const colorHandler = (setEvent) => {
+  switch (setEvent) {
+    case "gray":
+      setColor("color__home");
+      hideMenu();
+      setText("gray");
+      break;
+    case "red":
+      setColor("color__red");
+      hideMenu();
+      setText("red");
+      break;
+    case "orange":
+      setColor("color__orange");
+      hideMenu();
+      setText("orange");
+      break;
+    case "yellow":
+      setColor("color__yellow");
+      hideMenu();
+      setText("yellow");
+      break;
+    case "green":
+      setColor("color__green");
+      hideMenu();
+      setText("green");
+      break;
+    case "blue":
+      setColor("color__blue");
+      hideMenu();
+      setText("blue");
+      break;
+    case "purple":
+      setColor("color__purple");
+      hideMenu();
+      setText("purple");
+      break;
+    default:
+      setColor("color__home");
+      hideMenu();
+      setText("gray");
+      break;
+  }
+};
+
+colorHandler();
 
 colorHome.addEventListener("click", function () {
-  setColor("color__home");
-  hideMenu();
+  colorHandler("gray");
 });
 colorRed.addEventListener("click", function () {
-  setColor("color__red");
-  hideMenu();
+  colorHandler("red");
 });
 colorOrange.addEventListener("click", function () {
-  setColor("color__orange");
-  hideMenu();
+  colorHandler("orange");
 });
 colorYellow.addEventListener("click", function () {
-  setColor("color__yellow");
-  hideMenu();
+  colorHandler("yellow");
 });
 colorGreen.addEventListener("click", function () {
-  setColor("color__green");
-  hideMenu();
+  colorHandler("green");
 });
 colorBlue.addEventListener("click", function () {
-  setColor("color__blue");
-  hideMenu();
+  colorHandler("blue");
 });
 colorPurple.addEventListener("click", function () {
-  setColor("color__purple");
-  hideMenu();
+  colorHandler("purple");
+});
+document.addEventListener("keydown", (event) => {
+  keyHandler(event.key);
+  console.log(event.key);
 });
